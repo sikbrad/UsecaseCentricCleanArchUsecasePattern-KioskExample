a8cf855577ca6111bacf14b36574968927d1cceb
sikbrad <sikbrad@gmail.com>
2020-12-06 22:02:17 +0900

    rem unwanted includes

--- dataproviders/src/test/java/com/gqshop/kiosk/dataprovider/database/foodmenu/FoodMenuDatabaseJdbcDataProviderTest.java
+++ dataproviders/src/test/java/com/gqshop/kiosk/dataprovider/database/foodmenu/FoodMenuDatabaseJdbcDataProviderTest.java
@@ -4,9 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
