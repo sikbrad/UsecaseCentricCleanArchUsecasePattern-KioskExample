36e3f58f0e14bd39c91085654f2d4d36fbf82f0b
sikbrad <sikbrad@gmail.com>
2020-12-08 05:45:20 +0900

    1.logger lib import error fixed when integ testing

--- integration-tests/src/test/java/com/gqshop/kiosk/integration/database/DatabaseIntegrationTest.java
+++ integration-tests/src/test/java/com/gqshop/kiosk/integration/database/DatabaseIntegrationTest.java
@@ -2,6 +2,8 @@ package com.gqshop.kiosk.integration.database;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
--- integration-tests/src/test/java/com/gqshop/kiosk/integration/database/customer_ordering/CustomerOrderingEntrypointRestIntegrationTest.java
+++ integration-tests/src/test/java/com/gqshop/kiosk/integration/database/customer_ordering/CustomerOrderingEntrypointRestIntegrationTest.java
@@ -16,6 +16,8 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
